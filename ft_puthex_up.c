db46f87110ab690df59b8ff820a441291d4649d8
Dowiw <monjie12272@gmail.com>
2025-07-16 17:20:36 +0200

    add: hex conversions

---
+++ ft_puthex_up.c
@@ -0,0 +1,35 @@
+#include "ft_printf.h"
+
+int ft_puthex_upcase(unsigned int n)
+{
+	char			buffer[16];
+	int				i;
+	int				out;
+	unsigned int	temp;
+
+	i = 0;
+	out = 0;
+	temp = 0;
+	while (n != 0)
+	{
+		temp = n % 16;
+		if (temp < 10)
+			buffer[i++] = temp + '0';
+		else
+			buffer[i++] = temp - 10 + 'A';
+		n /= 16;
+	}
+	while (i--)
+		out += ft_putchar(buffer[i]);
+	return (out);
+}
+
+// int main() {
+// 	#include <stdio.h>
+// 	unsigned int test = 255;
+// 	printf("%i\n", test % 16);
+
+// 	int n = ft_puthex_upcase(test);
+// 	printf("\nn: %i\n", n);
+// 	return (0);
+// }
